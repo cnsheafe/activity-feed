8e9b57ffe0554014b80c12467fa56950ace92116
cnsheafe <cnsheafe@gmail.com>
2017-11-14 10:51:14 -0700

    Add conditional rendering and computed property
    Conditional rendering used to display either the actor or activity from the object. Using a computed property to avoid verbose expressions in template.

--- src/components/StatementCard.vue
+++ src/components/StatementCard.vue
@@ -4,7 +4,8 @@
     <div class="statement-text">
       <a :href="content.actor.mbox">{{content.actor.name}}</a>
       <span>{{content.verb.display}}</span>
-
+      <a v-if="content.object.objectType==='Agent'" :href="content.object.mbox">{{content.object.name}}</a>
+      <span v-else-if="content.object.objectType==='Activity'">{{activityName}}</span>
     </div>
 
   </div>
@@ -14,5 +15,13 @@
 export default {
   name: 'statement',
   props: ['content'],
+  computed: {
+    activityName() {
+      if (this.content.object.objectType === 'Activity') {
+        const objectName = this.content.object.definition.name;
+        return objectName[Object.keys(objectName)[0]];
+      }
+    },
+  },
 };
 </script>
